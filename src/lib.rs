ef4b1180c8dca6097f356d421297c7ddbd22a0b0
Patrick Elsen <pelsen@xfbs.net>
2022-12-28 14:30:07 -0500

    Updates documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,70 @@
+//! # MacroDB
+//!
+//! This crate exports a macro that can be used to turn an appropriate Rust struct into an
+//! in-memory database that guarantees consistency and supports indices. The Rust struct needs
+//! to be written by hand, the macro only generates a safe interface for using the in-memory
+//! database.
+//!
+//! ## Example
+//!
+//! Here is a full example of building a user database. An error type is defined, that is used
+//! by the database methods to return information failures. A struct for the record type that is
+//! to be stored is defined (the User struct). Finally, a struct for the database is defined
+//! that contains two indices. The [table][macro@table] macro generates methods for inserting, updating
+//! and deleting rows.
+//!
+//! ```rust
+//! use std::collections::{BTreeMap, BTreeSet};
+//! use macrodb::table;
+//!
+//! /// Error type for database interactions.
+//! pub enum Error {
+//!     UserIdExists,
+//!     UserEmailExists,
+//!     UserNotFound,
+//! }
+//!
+//! type UserId = u64;
+//!
+//! /// Record type for a user (a row in the database).
+//! #[derive(Clone)]
+//! pub struct User {
+//!     id: UserId,
+//!     name: String,
+//!     email: String,
+//! }
+//!
+//! /// Database definition.
+//! pub struct Database {
+//!     /// Users table.
+//!     users: BTreeMap<UserId, User>,
+//!     /// Unique index of users by email.
+//!     user_by_email: BTreeMap<String, UserId>,
+//! }
+//!
+//! // The table macro will autogenerate the users_insert(), users_update() and users_delete()
+//! // methods.
+//! impl Database {
+//!     table!(
+//!         users: User,
+//!         id: UserId,
+//!         missing Error => Error::UserNotFound,
+//!         primary users id => Error::UserIdExists,
+//!         unique user_by_email email => Error::UserEmailExists
+//!     );
+//! }
+//! ```
+//!
+//! See the documentation on [table](macro@table) for more information.
 #![macro_use]
+
+/// Re-expport of paste, which is used internally.
 pub use paste::paste;
 
 #[cfg(test)]
 mod tests;
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_next_id {
     ($table:ident: $type:ty) => {
@@ -19,6 +80,7 @@ macro_rules! table_next_id {
     };
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_insert {
     ($table:ident: $type:ty, $pk:ident, $errty:ty) => {
@@ -33,6 +95,7 @@ macro_rules! table_insert {
     };
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_insert_check {
     ($self:expr, foreign, $table:ident, $expr:expr, $err:expr) => {
@@ -54,6 +117,7 @@ macro_rules! table_insert_check {
     ($self:expr, index, $table:ident, $expr:expr, $err:expr) => {};
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_insert_checks {
     ($table:ident: $type:ty, $errty:ty, $($name:ident $ctable:ident $val:ident => $err:expr),*) => {
@@ -66,6 +130,7 @@ macro_rules! table_insert_checks {
     }
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_delete {
     ($table:ident: $type:ty, $pk:ty, $errty:ty) => {
@@ -80,6 +145,7 @@ macro_rules! table_delete {
     };
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_insert_index {
     ($self:expr, $pk:expr, unique, $name:ident, $prop:expr) => {
@@ -97,6 +163,7 @@ macro_rules! table_insert_index {
     ($self:expr, $pk:expr, foreign, $name:ident, $prop:expr) => {};
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_insert_indices {
     ($table:ident: $type:ty, $pk:ident, $($itype:ident $name:ident $prop:ident => $err:expr),*) => {
@@ -108,6 +175,7 @@ macro_rules! table_insert_indices {
     }
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_delete_index {
     ($self:expr, $pk:expr, unique, $name:ident, $prop:expr) => {
@@ -143,6 +211,7 @@ macro_rules! table_delete_index {
     ($self:expr, $pk:expr, foreign, $name:ident, $prop:expr) => {};
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_delete_indices {
     ($table:ident: $type:ty, $pk:ident, $($itype:ident $name:ident $prop:ident => $err:expr),*) => {
@@ -154,6 +223,7 @@ macro_rules! table_delete_indices {
     }
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_update_index {
     ($self:expr, $pk:expr, index, $name:ident, $old:expr, $new:expr) => {
@@ -178,6 +248,7 @@ macro_rules! table_update_index {
     ($self:expr, $pk:expr, primary, $name:ident, $old:expr, $new:expr) => {};
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_update_indices {
     ($table:ident: $type:ty, $pk:ident, $($itype:ident $name:ident $prop:ident => $err:expr),*) => {
@@ -189,6 +260,7 @@ macro_rules! table_update_indices {
     }
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_update_check {
     ($self:expr, $pk:expr, unique, $name:ident, $old:expr, $new:expr, $err:expr) => {
@@ -206,6 +278,7 @@ macro_rules! table_update_check {
     ($self:expr, $pk:expr, index, $name:ident, $old:expr, $new:expr, $err:expr) => {};
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_update_checks {
     ($table:ident: $type:ty, $pk:ident, $errty:ty, $($itype:ident $name:ident $prop:ident => $err:expr),*) => {
@@ -218,6 +291,7 @@ macro_rules! table_update_checks {
     }
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_delete_check {
     ($self:expr, $pk:expr, unique, $name:ident, $prop:expr, $err:expr) => {};
@@ -236,6 +310,7 @@ macro_rules! table_delete_check {
     ($self:expr, $pk:expr, index, $name:ident, $prop:expr, $err:expr) => {};
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_delete_checks {
     ($table:ident: $type:ty, $pk:ident: $pkty:ty, $errty:ty, $missing:expr, $($itype:ident $name:ident $prop:ident => $err:expr),*) => {
@@ -254,6 +329,7 @@ macro_rules! table_delete_checks {
     };
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_indices {
     ($table:ident: $type:ty, $pk:ident: $pkty:ty, $errty:ty, $error:expr, $($itype:ident $name:ident $prop:ident => $err:expr),*) => {
@@ -266,6 +342,7 @@ macro_rules! table_indices {
     }
 }
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! table_update {
     ($table:ident: $type:ty, $pk:ident => $err:expr, $errty:ty) => {
@@ -284,6 +361,240 @@ macro_rules! table_update {
     };
 }
 
+/// # Table Macro
+///
+/// Generate database methods (insert, update and delete) for a table.
+/// This macro takes a definition of the database table and indices and generates helper
+/// methods used to safely insert, update and delete rows in the database.
+///
+/// ## Usage
+///
+/// In order to use the macro, you need an error type for your database,
+/// a row type per table (for example, a User struct for your users table), and a struct
+/// for the database itself.
+///
+/// ### Error Type
+///
+/// The error type is usually a simple enum. For every table, you need an error case for when a
+/// row is missing, as well as when it exists. Additionally, you need one error type per unique
+/// index.
+///
+/// Here is an example of the what the error type might look like:
+///
+/// ```rust
+/// pub enum Error {
+///     UserIdExists,
+///     UserEmailExists,
+///     UserNotFound,
+///     GroupNameExists,
+///     GroupIdExists,
+///     GroupNotFound,
+///     GroupNotEmpty,
+/// }
+/// ```
+///
+/// ### Row Types
+///
+/// For every table, you need a row type. This can just be a regular Rust struct. It needs to
+/// derive [Clone][], and it needs to have some kind of primary key. The primary key uniquely
+/// identifies the row for update and deletion operations. Usually, an integer type is
+/// recommended here. For visual clarity, you can create a type alias for this field. We will
+/// refer to the row type as `RowType` and to the type of the primary key as `RowId`.
+///
+/// Here is an example of what *User* and *Group* row types might look like.
+///
+/// ```rust
+/// type UserId = u64;
+///
+/// #[derive(Clone)]
+/// pub struct User {
+///     id: UserId,
+///     name: String,
+///     email: String,
+///     group: GroupId,
+/// }
+///
+/// type GroupId = u64;
+///
+/// #[derive(Clone)]
+/// pub struct Group {
+///     id: GroupId,
+///     name: String,
+/// }
+/// ```
+///
+///
+/// ### Database Struct
+///
+/// The database struct must contain one map per table and one per index.
+/// The table maps must be of the shape `MapType<RowPrimaryKey, RowType>`. The type of map that is used does not matter, although common choices are [BTreeMap][std::collections::BTreeMap]
+/// and [HashMap][std::collections::HashMap].
+/// For every unique index, a map of the shape `MapType<IndexType,
+/// RowId>` must be added.
+/// For every index, a map of the shape `MapType<IndexType, Set<RowId>>`
+/// needs to be added.
+/// The set type that is used
+/// does not matter, although common choices are [BTreeSet][std::collections::BTreeSet]
+/// and [HashSet][std::collections::HashSet].
+/// The *IndexType* is the type of the field of the index. For instance, if
+/// the index is on a field of type [String][], that is the IndexType.
+///
+/// | Kind | Type |
+/// | --- | --- |
+/// | Table | `MapType<RowId, RowType>` |
+/// | Index | `MapType<IndexType, Set<RowId>>` |
+/// | Unique index | `MapType<IndexType, RowId>` |
+///
+/// For example, to define a database struct with two tables (*users* and *groups*),
+/// two unique indices (*user_by_email* and *group_by_name*), and one regular index
+/// (*users_by_group*), this struct definition could be used:
+///
+/// ```rust
+/// # type UserId = u64;
+/// # type GroupId = u64;
+/// # struct User;
+/// # struct Group;
+/// use std::collections::{BTreeMap, HashMap, BTreeSet, HashSet};
+///
+/// pub struct Database {
+///     /// Users table
+///     users: BTreeMap<UserId, User>,
+///     /// User by email unique index
+///     user_by_email: HashMap<String, UserId>,
+///
+///     /// Groups table
+///     groups: HashMap<GroupId, Group>,
+///     /// Users by group index
+///     users_by_group: BTreeMap<GroupId, BTreeSet<UserId>>,
+///     /// Group by name unique index
+///     group_by_name: BTreeMap<String, GroupId>,
+/// }
+/// ```
+///
+/// ## Syntax
+///
+/// The basic syntax of the macro looks like this:
+///
+/// ```rust,ignore
+/// table!(
+///     $table_name: RowType,
+///     $id_field: RowId,
+///     missing ErrorType => $missing_error,
+///     primary $table_name $id_field => $exists_error,
+///     <indices...>
+/// );
+/// ```
+///
+/// Here is an overview of what the various placeholders mean:
+///
+/// | Placeholder | Example | Explanation |
+/// | --- | --- | --- |
+/// | `$table_name` | `users` | Name of the table map in the database struct |
+/// | `RowType` | `User` | Name of the data type of the rows |
+/// | `RowId` | `UserId` | Name of the type of the primary keys for the rows |
+/// | `$id_field` | `id` | Name of the struct field of the Row type that contains the primary key |
+/// | `ErrorType` | `Error` | Name of the error type (enum) |
+/// | `$missing_error` | `Error::UserNotFound` | Error to throw when trying to delete a row that does not exists |
+/// | `$exists_error` | `Error::UserIdExists` | Error to throw when trying to insert a row that already exists |
+/// | `<indices...>` | | Definitions for the indices (explained in next section) |
+///
+/// The macro also needs to be told of the various indices. The syntax for indices looks like
+/// this:
+///
+/// ```text
+/// $type $map $field => $error
+/// ```
+///
+/// Here, `$type` refers to the type of index (can be `index`, `unique`, `foreign` or
+/// `reverse`). `$map` is the name of the map field in the database struct that represents this
+/// index. `$field` is the name of the field of the `RowType` that this index is on. Finally,
+/// `$error` is the error that is thrown when this index is violated. Here is an overview of the
+/// index types:
+///
+/// | Type | Example | Explanation |
+/// | --- | --- | --- |
+/// | Index | `index users_by_group group => ()` | Defines a simple index to look up rows based on their group. Does not need an error. |
+/// | Foreign | `foreign groups group => Error::GroupNotFound` | Defines a foreign key constraint which enforces that the `group` field point to an existing row in the `groups` table. |
+/// | Unique | `unique user_by_email email => Error::UserEmailExists` | Defines a unique index which uses the `user_by_email` map and enforces that no two users share the same email. |
+/// | Reverse | `reverse users_by_group id => Error::GroupHasUsers` | Declares a reverse dependency (on an index by another table) that prevents a group row being deleted if there are still users with that group. |
+///
+/// The result of this is that the macro generates insertion, update and deletion methods for
+/// every table. It uses the table map name as the prefix for those methods. For example,
+/// calling it on a table with the name *users* results in three methods being generated:
+///
+/// ```rust,ignore
+/// impl Database {
+///     /// Insert a User into the database, or return an error.
+///     pub fn users_insert(row: User) -> Result<(), Error>;
+///
+///     /// Update a User row (identified by the primary key), returning the old row, or return
+///     /// an error.
+///     pub fn users_update(row: User) -> Result<User, Error>;
+///
+///     /// Delete a User row (identified by the primary key), returning the row, or return an
+///     /// error.
+///     pub fn users_delete(id: UserId) -> Result<User, Error>;
+/// }
+/// ```
+///
+/// ## Example
+///
+/// Here is an example invocation of the macro on the Database struct with two tables (*users* and
+/// *groups*), including indices and foreign key constraints:
+///
+/// ```rust
+/// # use std::collections::{BTreeMap, HashMap, BTreeSet, HashSet};
+/// use macrodb::table;
+/// # pub enum Error {
+/// #     UserIdExists,
+/// #     UserEmailExists,
+/// #     UserNotFound,
+/// #     GroupNameExists,
+/// #     GroupIdExists,
+/// #     GroupNotFound,
+/// #     GroupNotEmpty,
+/// # }
+/// # type UserId = u64;
+/// # #[derive(Clone)]
+/// # pub struct User {
+/// #     id: UserId,
+/// #     name: String,
+/// #     email: String,
+/// #     group: GroupId,
+/// # }
+/// # type GroupId = u64;
+/// # #[derive(Clone)]
+/// # pub struct Group {
+/// #     id: GroupId,
+/// #     name: String,
+/// # }
+/// # pub struct Database {
+/// #     users: BTreeMap<UserId, User>,
+/// #     user_by_email: HashMap<String, UserId>,
+/// #     groups: HashMap<GroupId, Group>,
+/// #     users_by_group: BTreeMap<GroupId, BTreeSet<UserId>>,
+/// #     group_by_name: BTreeMap<String, GroupId>,
+/// # }
+/// impl Database {
+///     table!(
+///         users: User,
+///         id: UserId,
+///         missing Error => Error::UserNotFound,
+///         primary users id => Error::UserIdExists,
+///         foreign groups group => Error::GroupNotFound,
+///         index users_by_group group => (),
+///         unique user_by_email email => Error::UserEmailExists
+///     );
+///     table!(
+///         groups: Group,
+///         id: GroupId,
+///         missing Error => Error::GroupNotFound,
+///         primary users id => Error::GroupIdExists,
+///         reverse users_by_group id => Error::GroupNotEmpty,
+///         unique group_by_name name => Error::GroupNameExists
+///     );
+/// }
+/// ```
 #[macro_export]
 macro_rules! table {
     ($table:ident: $type:ty, $pk:ident: $pkty:ty, missing $errty:ty => $missing:expr, $($itype:ident $name:ident $prop:ident => $err:expr),*) => {
