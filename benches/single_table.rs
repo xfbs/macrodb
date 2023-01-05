fc6c904a356dea1a732de0b86255d038b2f743a5
Patrick Elsen <pelsen@xfbs.net>
2023-01-05 08:11:33 -0500

    Adds benchmark for im crate data structures

--- benches/single_table.rs
+++ benches/single_table.rs
@@ -1,5 +1,6 @@
 use btree_slab::{BTreeMap as BTreeSlabMap, BTreeSet as BTreeSlabSet};
 use criterion::*;
+use im::{HashMap as HashMapIm, OrdMap};
 use macrodb::table;
 use std::collections::{BTreeMap, BTreeSet};
 use std::collections::{HashMap, HashSet};
@@ -72,6 +73,26 @@ impl BTreeSlabDatabase {
     );
 }
 
+#[derive(Default, Clone)]
+struct OrdMapDatabase {
+    users: OrdMap<UserId, User>,
+    user_by_email: OrdMap<String, UserId>,
+    users_by_age: OrdMap<u32, BTreeSet<UserId>>,
+    users_by_name: OrdMap<String, BTreeSet<UserId>>,
+}
+
+impl OrdMapDatabase {
+    table!(
+        users: User,
+        id: UserId,
+        missing Error => Error::UserNotFound,
+        primary users id => Error::UserIdExists,
+        unique user_by_email email => Error::UserEmailExists,
+        index users_by_age age => (),
+        index users_by_name name => ()
+    );
+}
+
 #[derive(Default)]
 struct HashDatabase {
     users: HashMap<UserId, User>,
@@ -81,6 +102,26 @@ struct HashDatabase {
 }
 
 impl HashDatabase {
+    table!(
+        users: User,
+        id: UserId,
+        missing Error => Error::UserNotFound,
+        primary users id => Error::UserIdExists,
+        unique user_by_email email => Error::UserEmailExists,
+        index users_by_age age => (),
+        index users_by_name name => ()
+    );
+}
+
+#[derive(Default)]
+struct HashImDatabase {
+    users: HashMapIm<UserId, User>,
+    user_by_email: HashMapIm<String, UserId>,
+    users_by_age: HashMapIm<u32, HashSet<UserId>>,
+    users_by_name: HashMapIm<String, HashSet<UserId>>,
+}
+
+impl HashImDatabase {
     table!(
         users: User,
         id: UserId,
@@ -103,211 +144,115 @@ fn generate_users(limit: usize) -> Vec<User> {
         .collect::<Vec<User>>()
 }
 
+macro_rules! table_benchmark {
+    ($c:expr, $name:expr, $database:ty, $insertions:expr, $updates:expr, $deletions:expr) => {
+        let mut group = $c.benchmark_group($name);
+        for insertions in $insertions.into_iter() {
+            group.throughput(Throughput::Elements(insertions));
+            group.bench_with_input(format!("insert-{insertions}"), &insertions, |b, elems| {
+                b.iter_batched(
+                    || generate_users(*elems as usize),
+                    |data| {
+                        let mut database = <$database>::default();
+                        for user in data.into_iter() {
+                            database.users_insert(user).unwrap();
+                        }
+                        black_box(database)
+                    },
+                    BatchSize::SmallInput,
+                )
+            });
+        }
+        for updates in $updates.into_iter() {
+            group.throughput(Throughput::Elements(updates));
+            group.bench_with_input(format!("update-{updates}"), &updates, |b, elems| {
+                b.iter_batched(
+                    || {
+                        let users = generate_users(*elems as usize);
+                        let mut database = <$database>::default();
+                        for user in users.into_iter() {
+                            database.users_insert(user).unwrap();
+                        }
+                        database
+                    },
+                    |mut database| {
+                        for id in 0..*elems {
+                            let mut user = database.users.get(&id).unwrap().clone();
+                            user.age = 18 + (id % 53) as u32;
+                            user.name = format!("new-{id}");
+                            user.email = format!("new-{id}@example.com");
+                            database.users_update(user).unwrap();
+                        }
+                        black_box(database)
+                    },
+                    BatchSize::SmallInput,
+                )
+            });
+        }
+        for deletions in $deletions.into_iter() {
+            group.throughput(Throughput::Elements(deletions));
+            group.bench_with_input(format!("delete-{deletions}"), &deletions, |b, elems| {
+                b.iter_batched(
+                    || {
+                        let users = generate_users(*elems as usize);
+                        let mut database = <$database>::default();
+                        for user in users.into_iter() {
+                            database.users_insert(user).unwrap();
+                        }
+                        database
+                    },
+                    |mut database| {
+                        for id in 0..*elems {
+                            database.users_delete(id).unwrap();
+                        }
+                        black_box(database)
+                    },
+                    BatchSize::SmallInput,
+                )
+            });
+        }
+        group.finish();
+    };
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
     let insertions = [100_000];
     let deletions = [100_000];
     let updates = [100_000];
 
-    let mut group = c.benchmark_group("btree");
-    for insertions in insertions.into_iter() {
-        group.throughput(Throughput::Elements(insertions));
-        group.bench_with_input(format!("insert-{insertions}"), &insertions, |b, elems| {
-            b.iter_batched(
-                || generate_users(*elems as usize),
-                |data| {
-                    let mut database = BTreeDatabase::default();
-                    for user in data.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    for updates in updates.into_iter() {
-        group.throughput(Throughput::Elements(updates));
-        group.bench_with_input(format!("update-{updates}"), &updates, |b, elems| {
-            b.iter_batched(
-                || {
-                    let users = generate_users(*elems as usize);
-                    let mut database = BTreeDatabase::default();
-                    for user in users.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    database
-                },
-                |mut database| {
-                    for id in 0..*elems {
-                        let mut user = database.users.get(&id).unwrap().clone();
-                        user.age = 18 + (id % 53) as u32;
-                        user.name = format!("new-{id}");
-                        user.email = format!("new-{id}@example.com");
-                        database.users_update(user).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    for deletions in deletions.into_iter() {
-        group.throughput(Throughput::Elements(deletions));
-        group.bench_with_input(format!("delete-{deletions}"), &deletions, |b, elems| {
-            b.iter_batched(
-                || {
-                    let users = generate_users(*elems as usize);
-                    let mut database = BTreeDatabase::default();
-                    for user in users.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    database
-                },
-                |mut database| {
-                    for id in 0..*elems {
-                        database.users_delete(id).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    group.finish();
-
-    let mut group = c.benchmark_group("btree-slab");
-    for insertions in insertions.into_iter() {
-        group.throughput(Throughput::Elements(insertions));
-        group.bench_with_input(format!("insert-{insertions}"), &insertions, |b, elems| {
-            b.iter_batched(
-                || generate_users(*elems as usize),
-                |data| {
-                    let mut database = BTreeSlabDatabase::default();
-                    for user in data.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    for updates in updates.into_iter() {
-        group.throughput(Throughput::Elements(updates));
-        group.bench_with_input(format!("update-{updates}"), &updates, |b, elems| {
-            b.iter_batched(
-                || {
-                    let users = generate_users(*elems as usize);
-                    let mut database = BTreeSlabDatabase::default();
-                    for user in users.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    database
-                },
-                |mut database| {
-                    for id in 0..*elems {
-                        let mut user = database.users.get(&id).unwrap().clone();
-                        user.age = 18 + (id % 53) as u32;
-                        user.name = format!("new-{id}");
-                        user.email = format!("new-{id}@example.com");
-                        database.users_update(user).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    for deletions in deletions.into_iter() {
-        group.throughput(Throughput::Elements(deletions));
-        group.bench_with_input(format!("delete-{deletions}"), &deletions, |b, elems| {
-            b.iter_batched(
-                || {
-                    let users = generate_users(*elems as usize);
-                    let mut database = BTreeSlabDatabase::default();
-                    for user in users.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    database
-                },
-                |mut database| {
-                    for id in 0..*elems {
-                        database.users_delete(id).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    group.finish();
-
-    let mut group = c.benchmark_group("hash");
-    for insertions in insertions.into_iter() {
-        group.throughput(Throughput::Elements(insertions));
-        group.bench_with_input(format!("insert-{insertions}"), &insertions, |b, elems| {
-            b.iter_batched(
-                || generate_users(*elems as usize),
-                |data| {
-                    let mut database = HashDatabase::default();
-                    for user in data.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    for updates in updates.into_iter() {
-        group.throughput(Throughput::Elements(updates));
-        group.bench_with_input(format!("update-{updates}"), &updates, |b, elems| {
-            b.iter_batched(
-                || {
-                    let users = generate_users(*elems as usize);
-                    let mut database = HashDatabase::default();
-                    for user in users.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    database
-                },
-                |mut database| {
-                    for id in 0..*elems {
-                        let mut user = database.users.get(&id).unwrap().clone();
-                        user.age = 18 + (id % 53) as u32;
-                        user.name = format!("new-{id}");
-                        user.email = format!("new-{id}@example.com");
-                        database.users_update(user).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    for deletions in deletions.into_iter() {
-        group.throughput(Throughput::Elements(deletions));
-        group.bench_with_input(format!("delete-{deletions}"), &deletions, |b, elems| {
-            b.iter_batched(
-                || {
-                    let users = generate_users(*elems as usize);
-                    let mut database = HashDatabase::default();
-                    for user in users.into_iter() {
-                        database.users_insert(user).unwrap();
-                    }
-                    database
-                },
-                |mut database| {
-                    for id in 0..*elems {
-                        database.users_delete(id).unwrap();
-                    }
-                    black_box(database)
-                },
-                BatchSize::SmallInput,
-            )
-        });
-    }
-    group.finish();
+    table_benchmark!(
+        c,
+        "std::btree",
+        BTreeDatabase,
+        insertions,
+        updates,
+        deletions
+    );
+    table_benchmark!(c, "std::hash", HashDatabase, insertions, updates, deletions);
+    table_benchmark!(
+        c,
+        "slab::btree",
+        BTreeSlabDatabase,
+        insertions,
+        updates,
+        deletions
+    );
+    table_benchmark!(
+        c,
+        "im::btree",
+        OrdMapDatabase,
+        insertions,
+        updates,
+        deletions
+    );
+    table_benchmark!(
+        c,
+        "im::hash",
+        HashImDatabase,
+        insertions,
+        updates,
+        deletions
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
